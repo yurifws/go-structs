8eeceb9eacc853d31c8895d07a5dd36ec52745fb
yurifws <yurifws@gmail.com>
2025-12-09 00:32:35 -0300

    <feat GPL-47> structs with receiver functions

--- main.go
+++ main.go
@@ -24,6 +24,15 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.updateName("Felipe")
+	jim.print()
 
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
