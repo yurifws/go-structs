d3f003191db5e08d81cc23fd3575f62bcfbed050
yurifws <yurifws@gmail.com>
2025-12-09 00:03:48 -0300

    <feat GPL-44> declaring structs

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+	}
 
+	sofia := person{"Sofia", "Lee"}
+
+	fmt.Println(alex)
+	fmt.Println(sofia)
 }
